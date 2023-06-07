bf1e296272ab368a28b28219ce906b830dc2d7dd
PoisonGravity <107403926+PoisonGravity@users.noreply.github.com>
2023-06-07 14:37:34 +0100

    JUC基础 CountDownLatchDemo

--- src/main/java/com/example/threadpractice/add/CountDownLatchDemo.java
+++ src/main/java/com/example/threadpractice/add/CountDownLatchDemo.java
@@ -1,4 +1,22 @@
 package com.example.threadpractice.add;
 
+import java.util.concurrent.CountDownLatch;
+
 public class CountDownLatchDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        CountDownLatch countDownLatch = new CountDownLatch(6);
+
+        for (int i = 0; i < 6; i++) {
+            new Thread(()->{
+                System.out.println(Thread.currentThread().getName()+"Thread Get out");
+                countDownLatch.countDown();
+            }).start();
+        }
+
+        countDownLatch.await();
+
+        System.out.println("Close Door");
+    }
 }
